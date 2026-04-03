87c19ea280572876fc9ce33511ea98ed83caad0b
Kendalumana <kendalua23@gmail.com>
2026-04-03 17:25:37 -0600

    Division de css 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import markerIconPng from 'leaflet/dist/images/marker-icon.png';
 import markerShadowPng from 'leaflet/dist/images/marker-shadow.png';
-import './styles/index.css';
+import './styles/Index.css';
 
 // Datos
 import { PLACES, CATEGORIES } from './data/places';
